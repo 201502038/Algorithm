91c09f57c44ab2484442166aad8fb63e2163f7a3
201502038 <bnm961126@gmail.com>
2020-01-07 11:40:09 +0900

    solve dfs

--- src/com/company/_16_heap.java
+++ src/com/company/_16_heap.java
@@ -1,5 +1,33 @@
 package com.company;
 
-public class _16_heap {
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class _16_heap {
+    public static void main(String[] args) {
+        int stock = 4;
+        int[] dates = {4, 10, 15};
+        int[] supplies = {20, 5, 10};
+        int k = 30;
+        int answer = 0;
+
+        Queue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+
+        int index = 0;
+        for (int i = 0; i < k; i++) {
+            if(index < dates.length && i == dates[index])
+                priorityQueue.add(supplies[index++]);
+
+            if(stock == 0) {
+                stock += priorityQueue.poll();
+                answer++;
+            }
+            stock -= 1;
+        }
+        System.out.println(answer);
+
+
+    }
 }
---
+++ src/com/company/_17_DFS.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class _17_DFS {
+    static boolean[][] visit;
+
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] computers = {{1, 1, 1, 0}, {1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}};
+        int answer = 0;
+        visit = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i][i]) {
+                answer++;
+                dfs(i, computers, n);
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void dfs(int i, int[][] computers, int n) {
+        for (int j = 0; j < n; j++) {
+            if (computers[i][j] == 1 && !visit[i][j]){
+                visit[i][j] = visit[j][i] = true;
+                dfs(j,computers,n);
+            }
+        }
+    }
+}
