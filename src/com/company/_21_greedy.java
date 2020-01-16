fa76b4df9274c394e39d561567af73cb70e193f7
201502038 <bnm961126@gmail.com>
2020-01-16 10:58:34 +0900

    solve greedy

--- src/com/company/_21_greedy.java
+++ src/com/company/_21_greedy.java
@@ -1,18 +1,21 @@
 package com.company;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class _21_greedy {
     public static void main(String[] args) {
-        int answer = 0;
-        int[] people = {70, 50, 80, 50};
+        int answer = 1;
+        int[] people = {30, 50, 50, 70};
         int limit = 100;
 
-        List<Integer> p = Arrays.stream(people).boxed().collect(Collectors.toList());
-        p.sort(Comparator.reverseOrder());
+        Arrays.sort(people);
+
+        int index = people.length - 1;
+        for(int i = 0; i <= index; i++, answer++){
+            while(index > i && people[i] + people[index--] > limit)
+                answer++;
+        }
+
 
 
     }
