1876a02a9bd3e78f1bed303edd577fa06407317d
201502038 <bnm961126@gmail.com>
2020-01-20 17:34:00 +0900

    Queue

---
+++ src/com/company/_25_search.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class _25_search {
+    public static void main(String[] args) {
+        String begin = "hit";
+        String target = "cog";
+        String[] words = {"hot", "dot", " dog", "lot", " log", " cog"};
+        int answer = 0;
+
+    }
+
+    public static int solution(String begin, String target, String[] words) {
+
+    }
+}
