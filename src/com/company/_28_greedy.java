065bb806bc2e7e0c77d942f28f2f1246ab72823c
201502038 <bnm961126@gmail.com>
2020-02-08 14:44:03 +0900

    union find

--- src/com/company/_28_greedy.java
+++ src/com/company/_28_greedy.java
@@ -3,14 +3,19 @@ package com.company;
 import java.util.PriorityQueue;
 
 public class _28_greedy {
+    static int par[];
+
     public static void main(String[] args) {
         int n = 4;
-//        int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+//      int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
         int[][] costs = {{0, 1, 5}, {1, 2, 3}, {2, 3, 3}, {3, 1, 2}, {3, 0, 4}};
         int answer = 0;
 
         Edge[] edges = new Edge[costs.length];
-
+        par = new int[n];
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+        }
         for (int i = 0; i < costs.length; i++) {
             for (int j = 0; j < 3; j++) {
                 edges[i] = new Edge(costs[i][0], costs[i][1], costs[i][2], false);
@@ -23,17 +28,29 @@ public class _28_greedy {
         }
 
         while (!priorityQueue.isEmpty()) {
-            Edge e = priorityQueue.poll();
-            if () {
-                answer += e.cost;
+            Edge edge = priorityQueue.poll();
+            if (find(edge.src) != find(edge.des)) {
+                union(edge.src, edge.des);
+                answer += edge.cost;
             }
+
         }
 
         System.out.println(answer);
     }
-    public static boolean visit(Edge e){
 
-        return false;
+    private static int find(int a) {
+        if (par[a] == a) return a;
+        par[a] = find(par[a]);
+        return par[a];
+    }
+
+    private static void union(int a, int b) {
+        if (find(a) != find(b)) {
+            par[find(a)] = find(b);
+        } else {
+            return;
+        }
     }
 }
 
